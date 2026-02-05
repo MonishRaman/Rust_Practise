bccb2278b072d0bda567ac34280cf1f01aafb9e7
MonishRaman <monishr608@gmail.com>
2026-02-05 22:27:09 +0530

    Added

---
+++ primitive-data-types/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn data_types() -> (u8, f64, bool, char) {
+    // 1. Define variable of type `u8` and value `42`
+
+    // 2. Define variable of type `f64` and value `3.14`
+
+    // 3. Define variable of type `bool` and value `false`
+
+    // 4. Define variable of type `char` and value `a`
+
+    // 5. Return a tuple with the variables in the order they were defined
+pub fn data_types() -> (u8, f64, bool, char) {
+    let n: u8 = 42;
+    let fl: f64 = 3.14;
+    let bo: bool = false;
+    let c: char = 'a';
+
+    (n, fl, bo, c)
+}
+
+}
